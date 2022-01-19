edcf0c838dfcb3c6821657a7a3ac21251f275ed7
5CYTH3 <ScytheD@outlook.fr>
2022-01-19 21:42:12 +0100

    Deleted useless table creation. Will create it by hand later when the server is set up

--- src/lib/functions/db.ts
+++ src/lib/functions/db.ts
@@ -6,8 +6,3 @@ r.connect({host: 'localhost', port: 28015}, (err, conn) => {
     if(err) throw err;
     connection = conn;
 })
-
-r.db('dev').tableCreate('vehicles').run(connection, (err, res) => {
-    if (err) throw err;
-    console.log(JSON.stringify(res, null, 2))
-})
